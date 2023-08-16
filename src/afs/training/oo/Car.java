ec8b16b509b39c55bbd2e79c030d5bd8f8afa43b
yamboni1 <ncyambot.work@gmail.com>
2023-08-16 15:24:36 +0800

    feat: added functions for Car: coolCar

---
+++ src/afs/training/oo/Car.java
@@ -0,0 +1,18 @@
+package afs.training.oo;
+
+public class Car {
+    private final String name;
+    private int speed;
+    public Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+
+
+
+    }
+
+    public void speedUp(int acceleration) {
+        speed = speed + acceleration;
+        System.out.printf("%s: speed up to %d km/h", name,speed);
+    }
+}
---
+++ src/afs/training/oo/Main.java
@@ -0,0 +1,11 @@
+package afs.training.oo;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+           Car coolCar = new Car("Cool Car", 25);
+           coolCar.speedUp(5);
+        }
+
+}
